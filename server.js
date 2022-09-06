bae5680af231d29589d8d2a40904840836f7f036
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-09-05 21:18:40 -0700

    HTML routes added

--- server.js
+++ server.js
@@ -14,3 +14,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static("public"));
 
+// HTML routes //
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    );
+
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    );
+
