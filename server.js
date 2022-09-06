ce0905234db1d2a2b75990a6afafe84ced585a07
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-09-05 21:44:09 -0700

    write note added

--- server.js
+++ server.js
@@ -33,3 +33,21 @@ app.post('api/routes', (req, res) => {
     res.json(newNote);
 })
 
+// Function to create new note with corresponding id
+const createNote = (body, notesArray) => {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray.length;
+    notesArray[0]++;
+    notesArray.push(newNote);
+
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+};
