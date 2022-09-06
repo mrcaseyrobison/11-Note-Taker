f7b4f92d0caa212b3e6bf61ff42841f454bdfe04
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-09-05 21:16:59 -0700

    working on it

--- server.js
+++ server.js
@@ -8,3 +8,9 @@ const path = require('path')
 // Connect Port //
 const PORT = process.env || 30001;
 const app = express();
+
+// Express //
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static("public"));
+
